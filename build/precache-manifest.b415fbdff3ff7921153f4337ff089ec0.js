61b21533e86a119e219cb3875a4f9854b8ce6c2f
Tatiana Cherednichenko <t.cherednichenko@redmadrobot.com>
2020-06-02 21:46:08 +0300

    add project

---
+++ build/precache-manifest.b415fbdff3ff7921153f4337ff089ec0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f90ffce8cbdfdcf21f54732a5bd878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0b7552c96ebc5f85cb",
+    "url": "/static/css/main.64d346a9.chunk.css"
+  },
+  {
+    "revision": "79ea96671e18500447a6",
+    "url": "/static/js/2.1cb140d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1cb140d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0b7552c96ebc5f85cb",
+    "url": "/static/js/main.bcd0c7f9.chunk.js"
+  },
+  {
+    "revision": "18eba952d72eff5d1533",
+    "url": "/static/js/runtime-main.1db2f38d.js"
+  },
+  {
+    "revision": "734f3a90e253a0f742716784bc0ca70d",
+    "url": "/static/media/background.734f3a90.jpg"
+  },
+  {
+    "revision": "916c5209b93e354839da1ba9bf73a57b",
+    "url": "/static/media/melody.916c5209.svg"
+  }
+]);
